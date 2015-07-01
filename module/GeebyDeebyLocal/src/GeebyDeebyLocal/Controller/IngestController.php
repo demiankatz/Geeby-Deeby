f0d6a86467a551c6abba9ca811e2b86c604f79d1
Demian Katz <demian.katz@villanova.edu>
2015-07-01 11:15:28 -0400

    Began basic data extraction.

--- module/GeebyDeebyLocal/src/GeebyDeebyLocal/Controller/IngestController.php
+++ module/GeebyDeebyLocal/src/GeebyDeebyLocal/Controller/IngestController.php
@@ -46,11 +46,103 @@ class IngestController extends \GeebyDeeby\Controller\AbstractBase
      */
     public function indexAction()
     {
-        $mods = $this->getModsForEdition('http://dimenovels.org/Edition/5865');
-        if (!$mods) {
+        $edition = 5865;
+        $rawMods = $this->getModsForEdition('http://dimenovels.org/Edition/' . $edition);
+        if (!$rawMods) {
             Console::writeLine('Could not retrieve MODS.');
             return;
         }
+        $mods = simplexml_load_string($rawMods);
+        $editionObj = $this->getDbTable('edition')->getByPrimaryKey($edition);
+        $item = $this->getItemForEdition($editionObj);
+
+        $details = [];
+        foreach ($mods->xpath('/mods:mods') as $current) {
+            $currentDetails = $this->extractDetailsFromMods($current);
+            if (!empty($currentDetails)) {
+                $details[] = $currentDetails;
+            }
+        }
+        foreach ($mods->xpath('/mods:mods/mods:relatedItem[@type="constituent"]') as $current) {
+            $currentDetails = $this->extractDetailsFromMods($current);
+            if (!empty($currentDetails)) {
+                $details[] = $currentDetails;
+            }
+        }
+    }
+
+    protected function extractDetailsFromMods($mods)
+    {
+        $details = [];
+        $title = $this->extractTitleInfoFromMods($mods);
+        if (!empty($title)) {
+            $details['title'] = $title;
+        }
+        $authors = $this->extractAuthorsFromMods($mods);
+        if (!empty($authors)) {
+            $details['authors'] = $authors;
+        }
+        return $details;
+    }
+
+    protected function checkTitle($item, $title)
+    {
+        $itemTitle = (isset($item['Item_AltName']) && !empty($item['Item_AltName']))
+            ? $item['Item_AltName'] : $item['Item_Name'];
+        Console::writeLine('Comparing to ' . $itemTitle);
+    }
+
+    protected function extractAuthorsFromMods($mods)
+    {
+        $authors = [];
+        $matches = $mods->xpath('mods:name');
+        foreach ($matches as $current) {
+            $role = $current->xpath('mods:role/mods:roleTerm');
+            if (isset($role[0]) && (string)$role[0] == 'author') {
+                $currentAuthor = [];
+                $uri = $current->xpath('@valueURI');
+                if (isset($uri[0])) {
+                    $currentAuthor['uri'] = (string)$uri[0];
+                }
+                $currentAuthor['name'] = implode(', ', $current->xpath('mods:namePart'));
+                if (!empty($currentAuthor['name'])) {
+                    Console::writeLine(
+                        'Extracted author: ' . $currentAuthor['name']
+                        . (empty($currentAuthor['uri']) ? '' : " ({$currentAuthor['uri']})")
+                    );
+                    $authors[] = $currentAuthor;
+                }
+            }
+        }
+        return $authors;
+    }
+
+    protected function extractTitleInfoFromMods($mods)
+    {
+        $matches = $mods->xpath('mods:titleInfo[not(@type="alternative")]');
+        if (empty($matches)) {
+            return '';
+        }
+        $title = (string)$matches[0]->xpath('mods:title')[0];
+        $article = $matches[0]->xpath('mods:nonSort');
+        $full = $title .= (empty($article) ? '' : ', ' . (string)$article[0]);
+        Console::writeLine("Extracted title: $full");
+        return $full;
+    }
+
+    protected function getItemForEdition($rowObj)
+    {
+        $itemTable = $this->getDbTable('item');
+        $itemObj = $itemTable->getByPrimaryKey($rowObj->Item_ID);
+        $item = $itemObj->toArray();
+        if (!empty($rowObj->Preferred_Item_AltName_ID)) {
+            $ian = $this->getDbTable('itemsalttitles');
+            $tmpRow = $ian->select(
+                array('Sequence_ID' => $rowObj->Preferred_Item_AltName_ID)
+            )->current();
+            $item['Item_AltName'] = $tmpRow['Item_AltName'];
+        }
+        return $item;
     }
 
     protected function getModsForEdition($edition)
