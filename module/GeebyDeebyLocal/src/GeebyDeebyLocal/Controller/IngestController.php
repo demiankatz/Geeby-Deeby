ec68c777cb459b8129cd2072bca21426336d28fa
Demian Katz <demian.katz@villanova.edu>
2015-07-17 11:29:16 -0400

    Progress on validating/synchronizing data pulled from MODS.

--- module/GeebyDeebyLocal/src/GeebyDeebyLocal/Controller/IngestController.php
+++ module/GeebyDeebyLocal/src/GeebyDeebyLocal/Controller/IngestController.php
@@ -47,6 +47,11 @@ class IngestController extends \GeebyDeeby\Controller\AbstractBase
     public function indexAction()
     {
         $edition = 5865;
+        return $this->loadExistingEdition($edition);
+    }
+
+    protected function loadExistingEdition($edition)
+    {
         $rawMods = $this->getModsForEdition('http://dimenovels.org/Edition/' . $edition);
         if (!$rawMods) {
             Console::writeLine('Could not retrieve MODS.');
@@ -55,17 +60,99 @@ class IngestController extends \GeebyDeeby\Controller\AbstractBase
         $mods = simplexml_load_string($rawMods);
         $editionObj = $this->getDbTable('edition')->getByPrimaryKey($edition);
         $item = $this->getItemForEdition($editionObj);
+        $series = $this->getSeriesForEdition($editionObj);
 
         $extractor = new ModsExtractor();
         $details = $extractor->getDetails($mods);
         print_r($details);
+
+        if (!$this->validateSeries($details, $editionObj, $series)) {
+            Console::writeLine('Series validation failed.');
+            return;
+        }
+
+        $childDetails = $this->synchronizeChildren($editionObj, $details['contents']);
+        if (!$childDetails) {
+            return;
+        }
     }
 
-    protected function checkTitle($item, $title)
+    protected function synchronizeChildren($editionObj, $contents)
+    {
+        $lookup = $this->getDbTable('edition')
+            ->select(['Parent_Edition_ID' => $editionObj->Edition_ID]);
+        $children = [];
+        foreach ($lookup as $child) {
+            $children[] = [
+                'edition' => $child,
+                'item' => $this->getItemForEdition($child)
+            ];
+        }
+
+        $result = [];
+        foreach ($contents as $currentContent) {
+            $match = false;
+            foreach ($children as & $currentChild) {
+                if ($this->checkItemTitle($currentChild['item'], $currentContent['title'])) {
+                    $match = true;
+                    $result[] = [$currentContent, $currentChild];
+                    $currentChild['matched'] = true;
+                    break;
+                }
+            }
+            if (!$match) {
+                $result[] = [$currentContent, false];
+            }
+        }
+
+        // Fail if we have any existing data not matched up with new data....
+        foreach ($children as $child) {
+            if (!isset($child['matched'])) {
+                Console::writeLine("FATAL: No match found for edition {$child['edition']->Edition_ID}");
+                return false;
+            }
+        }
+        return $result;
+    }
+
+    protected function validateSeries($details, $editionObj, $series)
+    {
+        if (!isset($details['series'])) {
+            Console::writeLine('No series found.');
+            return false;
+        }
+        $expectedNumber = intval($editionObj->Position);
+        foreach ($details['series'] as $seriesName => $number) {
+            $actualNumber = intval(preg_replace('/[^0-9]/', '', $number));
+            Console::writeLine("Comparing {$expectedNumber} to {$actualNumber}...");
+            if ($actualNumber == $expectedNumber && $this->checkSeriesTitle($series, $seriesName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function fuzzyCompare($str1, $str2)
+    {
+        //Console::writeLine("Comparing {$str1} to {$str2}...");
+        $regex = '/[^a-z0-9]/';
+        $str1 = preg_replace($regex, '', strtolower($str1));
+        $str2 = preg_replace($regex, '', strtolower($str2));
+        return $str1 == $str2;
+    }
+
+    protected function checkItemTitle($item, $title)
     {
         $itemTitle = (isset($item['Item_AltName']) && !empty($item['Item_AltName']))
             ? $item['Item_AltName'] : $item['Item_Name'];
-        Console::writeLine('Comparing to ' . $itemTitle);
+        return $this->fuzzyCompare($title, $itemTitle);
+    }
+
+    protected function checkSeriesTitle($series, $title)
+    {
+        $seriesTitle = (isset($series['Series_AltName']) && !empty($series['Series_AltName']))
+            ? $series['Series_AltName'] : $series['Series_Name'];
+        return $this->fuzzyCompare($title, $seriesTitle);
     }
 
     protected function getItemForEdition($rowObj)
@@ -81,6 +168,21 @@ class IngestController extends \GeebyDeeby\Controller\AbstractBase
             $item['Item_AltName'] = $tmpRow['Item_AltName'];
         }
         return $item;
+    }
+
+    protected function getSeriesForEdition($rowObj)
+    {
+        $seriesTable = $this->getDbTable('series');
+        $seriesObj = $seriesTable->getByPrimaryKey($rowObj->Series_ID);
+        $series = $seriesObj->toArray();
+        if (!empty($rowObj->Preferred_Series_AltName_ID)) {
+            $san = $this->getDbTable('seriesalttitles');
+            $tmpRow = $san->select(
+                array('Sequence_ID' => $rowObj->Preferred_Series_AltName_ID)
+            )->current();
+            $series['Series_AltName'] = $tmpRow['Series_AltName'];
+        }
+        return $series;
     }
 
     protected function getModsForEdition($edition)
