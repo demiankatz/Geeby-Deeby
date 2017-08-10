54a93798855ab475465a30e67322bd9c9d1f8413
Demian Katz <demian.katz@villanova.edu>
2017-08-10 07:34:15 -0400

    Simplified platform code.

--- public/js/edit_platforms.js
+++ public/js/edit_platforms.js
@@ -1,68 +1,8 @@
-// Global reference to current open edit box.
-var editBox = false;
-
-/* Pop up a dialog to edit a platform:
- */
-function editPlatform(id)
-{
-    // Open the edit dialog box:
-    var url = basePath + '/edit/Platform/' + encodeURIComponent(id)
-    editBox = $('<div>Loading...</div>').load(url).dialog({
-        title: (id === 'NEW' ? "Add Platform" : ("Edit Platform " + id)),
-        modal: true,
-        autoOpen: true,
-        width: 500,
-        height: 400,
-        // Remove dialog box contents from the DOM to prevent duplicate identifier problems.
-        close: function() { $('#editForm').remove(); }
-    });
-}
-
-/* Redraw the platforms on the screen:
- */
-function redrawPlatforms()
-{
-    var url = basePath + '/edit/PlatformList';
-    $('#platform_list').load(url);
-}
-
-/* Save the platform inside the provided form element:
- */
-function savePlatform()
-{
-    // Obtain values from form:
-    var platformID = $('#Platform_ID').val();
-    var platform = $('#Platform').val();
-    
-    // Validate form:
-    if (platform.length == 0) {
-        alert('Platform cannot be blank.');
-        return;
-    }
-    
-    // Hide save button and display status message to avoid duplicate submission:
-    $('#save_platform').hide();
-    $('#save_platform_status').html('Saving...');
-    
-    // Use AJAX to save the values:
-    var url = basePath + '/edit/Platform/' + encodeURIComponent(platformID)
-    $.post(url, {platform: platform}, function(data) {
-        // If save was successful...
-        if (data.success) {
-            // Close the dialog box.
-            if (editBox) {
-                editBox.dialog('close');
-                editBox.dialog('destroy');
-                editBox = false;
-            }
-            
-            // Update the platform list.
-            redrawPlatforms();
-        } else {
-            // Save failed -- display error message and restore save button:
-            alert('Error: ' + data.msg);
-            $('#save_platform').show();
-            $('#save_platform_status').html('');
-        }
-    }, 'json');
-}
+var PlatformEditor = function() {
+    this.type = "Platform";
+    this.saveFields = {
+        'platform': { 'id': '#Platform', emptyError: 'Platform cannot be blank.' }
+    };
+};
+BaseEditor.prototype.registerSubclass(PlatformEditor);
+var Platform = new PlatformEditor();
