0a857285bb252f29412cd605be231bc0b2d24817
Demian Katz <demian.katz@villanova.edu>
2017-09-12 07:31:46 -0400

    Simplified publisher JS.

--- public/js/edit_publishers.js
+++ public/js/edit_publishers.js
@@ -3,6 +3,20 @@ var PublisherEditor = function() {
     this.saveFields = {
         'publisher': { 'id': '#Publisher_Name', emptyError: 'Publisher cannot be blank.' }
     };
+    this.links = {
+        'Address': {
+            'saveFields': {
+                'country': { 'id': '#Country_ID' },
+                'city': { 'id': '#City_ID' },
+                'street': { 'id': '#Street' }
+            }
+        },
+        'Imprint': {
+            'saveFields': {
+                'imprint': { 'id': '#Imprint', 'emptyError': 'Imprint must not be blank.' }
+            }
+        }
+    };
 };
 BaseEditor.prototype.registerSubclass(PublisherEditor);
 var Publisher = new PublisherEditor();
