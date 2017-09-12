8666c464ea65e412d09e4a823355d31c2fadf02f
Demian Katz <demian.katz@villanova.edu>
2017-09-12 07:43:57 -0400

    Began work to eliminate edit_series_details.js. - Simplified Materials code - Moved Item class customization

--- public/js/edit_series.js
+++ public/js/edit_series.js
@@ -6,6 +6,22 @@ var SeriesEditor = function() {
         'lang': { 'id': '#Language_ID' }
     };
     this.attributeSelector = '.series-attribute';
+    this.links = {
+        'Material': {
+            'uriField': { 'id': '#Series_Material_Type_ID' }
+        },
+    };
 };
 BaseEditor.prototype.registerSubclass(SeriesEditor);
 var Series = new SeriesEditor();
+
+/**
+ * Override the standard Item redraw list function:
+ */
+if (typeof Item === "object") {
+    Item.redrawList = function() {
+        var seriesID = $('#Series_ID').val();
+        var url = basePath + '/edit/Series/' + encodeURIComponent(seriesID) + '/Item';
+        $('#item_list').load(url);
+    }
+}
