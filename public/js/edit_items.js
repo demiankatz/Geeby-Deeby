5bfbd9011d5a92130a35a56c67dc0ae82701081e
Demian Katz <demian.katz@villanova.edu>
2017-09-12 08:06:04 -0400

    Safety check.

--- public/js/edit_items.js
+++ public/js/edit_items.js
@@ -83,9 +83,11 @@ var Item = new ItemEditor();
 
 // Load data and setup autocomplete.
 $(document).ready(function() {
-  registerAutocomplete('.Item_ID', 'Item');
-  registerAutocomplete('.Note_ID', 'Note');
-  registerAutocomplete('.Person_ID', 'Person');
-  registerAutocomplete('.Series_ID', 'Series');
-  registerAutocomplete('.Tag_ID', 'Tag');
+    if (typeof registerAutocomplete === 'function') {
+        registerAutocomplete('.Item_ID', 'Item');
+        registerAutocomplete('.Note_ID', 'Note');
+        registerAutocomplete('.Person_ID', 'Person');
+        registerAutocomplete('.Series_ID', 'Series');
+        registerAutocomplete('.Tag_ID', 'Tag');
+    }
 });
