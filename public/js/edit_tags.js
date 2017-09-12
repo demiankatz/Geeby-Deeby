618bbfa987f82033a35f4bc506b62e355571d2eb
Demian Katz <demian.katz@villanova.edu>
2017-09-12 17:27:37 -0400

    Simplified tag JS.

--- public/js/edit_tags.js
+++ public/js/edit_tags.js
@@ -4,6 +4,14 @@ var TagEditor = function() {
         'tag': { 'id': '#Tag', emptyError: 'Subject/tag name cannot be blank.' },
         'type_id': { 'id': '#Tag_Type_ID' }
     };
+    this.links = {
+        'URI': {
+            'uriField': { 'id': '#uri', 'emptyError': 'Please specify a valid URL.' },
+            'saveFields': {
+                'predicate_id': { 'id': '#Predicate_ID' }
+            }
+        }
+    };
 };
 BaseEditor.prototype.registerSubclass(TagEditor);
 var Tag = new TagEditor();
