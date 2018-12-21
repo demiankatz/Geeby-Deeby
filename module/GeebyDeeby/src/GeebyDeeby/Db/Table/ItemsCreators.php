0a719fe332271ae163f4f307497744d95d15993e
Demian Katz <demian.katz@villanova.edu>
2018-12-21 11:30:16 -0500

    Add citation listing on person view.

--- module/GeebyDeeby/src/GeebyDeeby/Db/Table/ItemsCreators.php
+++ module/GeebyDeeby/src/GeebyDeeby/Db/Table/ItemsCreators.php
@@ -49,6 +49,114 @@ class ItemsCreators extends Gateway
     }
 
     /**
+     * Get a list of credits attached to the specified person.
+     *
+     * @var int    $personID Person ID
+     * @var string $sort     Type of sorting (series/title/year)
+     *
+     * @return mixed
+     */
+    public function getCitationsForPerson($personID, $sort = 'series')
+    {
+        // Special case: bringing series into the mix makes things more complex:
+        if ($sort == 'series') {
+            return $this->getSeriesCitationsForPerson($personID);
+        }
+        return $this->getItemCitationsForPerson($personID, $sort);
+    }
+
+    /**
+     * Get a list of credits attached to the specified person, sorted by
+     * item.
+     *
+     * @var int    $personID Person ID
+     * @var string $sort     Type of sorting (title or year)
+     *
+     * @return mixed
+     */
+    public function getItemCitationsForPerson($personID, $sort = 'title')
+    {
+        $callback = function ($select) use ($personID, $sort) {
+            $count = new Expression(
+                'count(distinct(?))', array('icc.Citation_ID'),
+                array(Expression::TYPE_IDENTIFIER)
+            );
+            $select->join(
+                array('icc' => 'Items_Creators_Citations'),
+                'Items_Creators.Item_Creator_ID = icc.Item_Creator_ID',
+                array('Citation_Count' => $count), Select::JOIN_LEFT
+            );
+            $select->join(
+                array('i' => 'Items'), 'Items_Creators.Item_ID = i.Item_ID'
+            );
+            $select->join(
+                array('eds' => 'Editions'), 'eds.Item_ID = i.Item_ID'
+            );
+            $year = new Expression(
+                'min(?)', array('erd.Year'),
+                array(Expression::TYPE_IDENTIFIER)
+            );
+            $select->join(
+                array('erd' => 'Editions_Release_Dates'),
+                'eds.Edition_ID = erd.Edition_ID',
+                array('Earliest_Year' => $year), Select::JOIN_LEFT
+            );
+            $select->join(
+                array('r' => 'Roles'),
+                'Items_Creators.Role_ID = r.Role_ID'
+            );
+            $sortFields = $sort === 'year'
+                ? array('Role_Name', 'Earliest_Year', 'Item_Name')
+                : array('Role_Name', 'Item_Name', 'Earliest_Year');
+            $select->order($sortFields);
+            $select->group(array('Role_Name', 'Item_Name'));
+            $select->where->equalTo('Person_ID', $personID);
+        };
+        return $this->select($callback);
+    }
+
+    /**
+     * Get a list of credits attached to the specified person, sorted by
+     * series.
+     *
+     * @var int $personID Person ID
+     *
+     * @return mixed
+     */
+    public function getSeriesCitationsForPerson($personID)
+    {
+        $callback = function ($select) use ($personID) {
+            $select->join(
+                array('i' => 'Items'), 'Items_Creators.Item_ID = i.Item_ID'
+            );
+            $select->join(
+                array('eds' => 'Editions'), 'eds.Item_ID = i.Item_ID',
+                array('Edition_Name', 'Volume', 'Position', 'Replacement_Number')
+            );
+            $select->join(
+                array('iat' => 'Items_AltTitles'),
+                'eds.Preferred_Item_AltName_ID = iat.Sequence_ID',
+                array('Item_AltName'), Select::JOIN_LEFT
+            );
+            $select->join(
+                array('s' => 'Series'), 'eds.Series_ID = s.Series_ID'
+            );
+            $select->join(
+                array('r' => 'Roles'),
+                'Items_Creators.Role_ID = r.Role_ID'
+            );
+            $fields = array(
+                'Role_Name', 'Series_Name', 's.Series_ID', 'eds.Volume',
+                'eds.Position', 'eds.Replacement_Number', 'Item_Name'
+            );
+            $select->order($fields);
+            $select->group($fields);
+            $select->where->equalTo('Person_ID', $personID);
+        };
+        return $this->select($callback);
+    }
+
+    /**
      * Given an item identifier, return a list of creators.
      *
      * @param int $itemID Item ID
