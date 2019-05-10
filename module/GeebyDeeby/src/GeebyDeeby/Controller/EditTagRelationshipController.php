beab2f2e7af93a6d9d5772a00441a2fe0e421daa
Demian Katz <demian.katz@villanova.edu>
2019-05-10 09:54:24 -0400

    Fix typo in comment.

--- module/GeebyDeeby/src/GeebyDeeby/Controller/EditTagRelationshipController.php
+++ module/GeebyDeeby/src/GeebyDeeby/Controller/EditTagRelationshipController.php
@@ -28,7 +28,7 @@
 namespace GeebyDeeby\Controller;
 
 /**
- * Edit tag attribute controller
+ * Edit tag relationship controller
  *
  * @category GeebyDeeby
  * @package  Controller
@@ -64,7 +64,7 @@ class EditTagRelationshipController extends AbstractBase
             'priority' => 'Display_Priority',
             'inverse_relationship_name' => 'Tags_Inverse_Relationship_Name',
             'inverse_rdf_property' => 'Tags_Inverse_Relationship_RDF_Property',
-            'inverse_priority' => 'Inverse_Display_Priority',            
+            'inverse_priority' => 'Inverse_Display_Priority',
         );
         $response = $this
             ->handleGenericItem('tagsrelationship', $assignMap, 'relationship');
