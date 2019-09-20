7349fac68292b24334488e8581ecbf4f9e0fa966
Demian Katz <demian.katz@villanova.edu>
2019-09-20 10:57:52 -0400

    Create SQL for item attributes.

---
+++ data/migrations/20190920-001-add-item-attr.sql
@@ -0,0 +1,17 @@
+CREATE TABLE `Items_Attributes` (
+  `Items_Attribute_ID` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `Items_Attribute_Name` varchar(255) NOT NULL,
+  `Items_Attribute_RDF_Property` varchar(255),
+  `Allow_HTML` smallint(1) NOT NULL DEFAULT '0',
+  `Display_Priority` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Items_Attribute_ID`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `Items_Attributes_Values` (
+  `Item_ID` int(11) NOT NULL DEFAULT '0',
+  `Items_Attribute_ID` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `Items_Attribute_Value` varchar(32768) NOT NULL,
+  PRIMARY KEY (`Item_ID`, `Items_Attribute_ID`),
+  FOREIGN KEY (`Item_ID`) REFERENCES `Items` (`Item_ID`),
+  FOREIGN KEY (`Items_Attribute_ID`) REFERENCES `Items_Attributes` (`Items_Attribute_ID`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
