c1be14512ee398ff7340327766074e112331ce14
Demian Katz <demian.katz@villanova.edu>
2019-11-22 13:57:52 -0500

    Work in progress on editing interface (not working yet).

--- public/js/edit_countries.js
+++ public/js/edit_countries.js
@@ -3,6 +3,14 @@ var CountryEditor = function() {
     this.saveFields = {
         'country': { 'id': '#Country_Name', emptyError: 'Country cannot be blank.' }
     };
+    this.links = {
+        'URI': {
+            'uriField': { 'id': '#uri', 'emptyError': 'Please specify a valid URL.' },
+            'saveFields': {
+                'predicate_id': { 'id': '#Predicate_ID' }
+            }
+        }
+    };
 };
 BaseEditor.prototype.registerSubclass(CountryEditor);
 var Country = new CountryEditor();
@@ -11,6 +19,14 @@ var CityEditor = function() {
     this.type = "City";
     this.saveFields = {
         'city': { 'id': '#City_Name', emptyError: 'City cannot be blank.' }
+    };
+    this.links = {
+        'URI': {
+            'uriField': { 'id': '#uri', 'emptyError': 'Please specify a valid URL.' },
+            'saveFields': {
+                'predicate_id': { 'id': '#Predicate_ID' }
+            }
+        }
     };
 };
 BaseEditor.prototype.registerSubclass(CityEditor);
