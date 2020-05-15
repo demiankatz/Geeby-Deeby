6ff52defd9654c7ea152ee618fc69d6b6fb9a17d
Demian Katz <demian.katz@villanova.edu>
2020-05-15 16:34:22 -0400

    Allow items to be tagged from the tag view. (#71)

--- public/js/edit_tags.js
+++ public/js/edit_tags.js
@@ -6,6 +6,9 @@ var TagEditor = function() {
         'type_id': { 'id': '#Tag_Type_ID' }
     };
     this.links = {
+        'Item': {
+            'uriField': { 'id': '#Item_ID', 'nonNumericDefault': '', 'emptyError': 'Please specify a valid item.' }
+        },
         'Relationship': {
             'subtypeSelector': { 'id': '#relationship_type' },
             'targetSelector': '#relationship_list',
@@ -34,5 +37,6 @@ var TagType = new TagTypeEditor();
 $(document).ready(function() {
     if (typeof registerAutocomplete === 'function') {
         registerAutocomplete('#target_tag', 'Tag');
+        registerAutocomplete('#Item_ID', 'Item');
     }
 });
