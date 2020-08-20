8278a403798ae1bbdd4ffa8c1c6037dce944f04e
Demian Katz <demian.katz@villanova.edu>
2020-08-20 09:43:13 -0400

    Add missing comments.

--- module/GeebyDeebyLegacy/Module.php
+++ module/GeebyDeebyLegacy/Module.php
@@ -1,20 +1,58 @@
 <?php
 /**
- * Zend Framework (http://framework.zend.com/)
+ * Legacy Geeby-Deeby code module
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * PHP version 5
+ *
+ * Copyright (C) Demian Katz 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category GeebyDeeby
+ * @package  Modules
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/demiankatz/Geeby-Deeby Main Site
  */
 namespace GeebyDeebyLegacy;
 
+/**
+ * Legacy Geeby-Deeby code module
+ *
+ * @category GeebyDeeby
+ * @package  Modules
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/demiankatz/Geeby-Deeby Main Site
+ */
 class Module
 {
+    /**
+     * Get module configuration.
+     *
+     * @return array
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * Get autoloader configuration.
+     *
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
         return [
