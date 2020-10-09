c8ba670e3702eed19bda89a3008f43a91db2b3dd
Demian Katz <demian.katz@villanova.edu>
2020-10-09 14:03:23 -0400

    Fix bad namespace.

--- module/GeebyDeebyConsole/src/GeebyDeebyConsole/Command/Check/LinksCommand.php
+++ module/GeebyDeebyConsole/src/GeebyDeebyConsole/Command/Check/LinksCommand.php
@@ -25,7 +25,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://vufind.org/wiki/development Wiki
  */
-namespace GeebyDeeby\Command\Links;
+namespace GeebyDeeby\Command\Check;
 
 use GeebyDeeby\Db\Table\EditionsFullText;
 use Laminas\Http\Client;
