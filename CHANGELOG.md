0efa880cae1999b35dfdd72f4f691ddf34d5fdd0
Demian Katz <demian.katz@villanova.edu>
2020-10-09 14:19:37 -0400

    Add console command support (#79)
    - Add console module (derived from VuFind code, using Symfony/Console).
    - Add command for checking full text links and outputting CSV data

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,11 +6,13 @@ All notable changes to this project will be documented in this file, in reverse 
 
 ### Added
 
-- Nothing.
+- Command line interface for running tools (accessible through cli.php)
+- Command line utility for checking status of full text links.
 
 ### Changed
 
 - Migrated dependencies from Zend Framework to Laminas.
+- Minimum PHP version is now 7.1.3.
 - Renamed GeebyDeeby\Db\PluginManagerFactory to GeebyDeeby\ServiceManager\AbstractPluginManagerFactory for greater reusability.
 
 ### Removed
---
+++ module/GeebyDeebyConsole/Module.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Code module for Geeby-Deeby's console functionality
+ *
+ * PHP version 7
+ *
+ * Copyright (C) Demian Katz 2020.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * @category GeebyDeeby
+ * @package  Module
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/demiankatz/Geeby-Deeby Main Site
+ */
+namespace GeebyDeebyConsole;
+
+/**
+ * Code module for GeebyDeeby's console functionality
+ *
+ * @category GeebyDeeby
+ * @package  Module
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/demiankatz/Geeby-Deeby Main Site
+ */
+class Module
+{
+    /**
+     * Get module configuration
+     *
+     * @return array
+     */
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Get autoloader configuration
+     *
+     * @return array
+     */
+    public function getAutoloaderConfig()
+    {
+        return [
+            'Laminas\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ],
+            ],
+        ];
+    }
+}
