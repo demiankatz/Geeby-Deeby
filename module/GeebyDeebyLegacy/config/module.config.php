bd009b91985862c2c89f397909765381a390acd1
Demian Katz <demian.katz@villanova.edu>
2020-12-09 07:54:02 -0500

    Switch Zend to Laminas.

--- module/GeebyDeebyLegacy/config/module.config.php
+++ module/GeebyDeebyLegacy/config/module.config.php
@@ -1,17 +1,9 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return [
     'router' => [
         'routes' => [
             'legacy' => [
-                'type'    => 'Zend\Router\Http\Segment',
+                'type'    => 'Laminas\Router\Http\Segment',
                 'options' => [
                     'route'    => '/:filename',
                     'constraints' => [
--- module/GeebyDeebyLocal/config/module.config.php
+++ module/GeebyDeebyLocal/config/module.config.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * Zend Framework (http://framework.zend.com/)
- *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
- */
-
 return [
     'geeby-deeby' => [
         'siteTitle' => 'Demian\'s Gamebook Web Page',
@@ -21,7 +13,7 @@ return [
     'router' => [
         'routes' => [
             'thanks' => [
-                'type' => 'Zend\Router\Http\Literal',
+                'type' => 'Laminas\Router\Http\Literal',
                 'options' => [
                     'route'    => '/Thanks',
                     'defaults' => [
