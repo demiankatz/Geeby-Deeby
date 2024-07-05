4d8bb367bd37e4f5cca4f3a56fe5f11f5cfcf26a
Demian Katz <demian.katz@villanova.edu>
2024-07-05 10:38:52 -0400

    Add edition attribute display.:

--- module/GeebyDeeby/src/GeebyDeeby/Controller/SearchController.php
+++ module/GeebyDeeby/src/GeebyDeeby/Controller/SearchController.php
@@ -81,6 +81,24 @@ class SearchController extends AbstractBase
     }
 
     /**
+     * Retrieve edition attribute details by AJAX.
+     *
+     * @return mixed
+     */
+    public function editionAttributeAjaxAction()
+    {
+        $post = $this->params()->fromPost();
+        $attributeId = $post['attribute_id'];
+        $ids = explode(',', $post['ids']);
+        $result = [];
+        foreach ($this->getDbTable('editionsattributesvalues')->getAttributesForItem($ids, $attributeId) as $row) {
+            // We only want to display one value per item, so it doesn't matter if we overwrite existing data here:
+            $result[$row['Item_ID']] = '<b>' . htmlspecialchars($row['Editions_Attribute_Name']) . '</b>: ' . htmlspecialchars($row['Editions_Attribute_Value']);
+        }
+        return $this->jsonDie($result, true);
+    }
+
+    /**
      * Keyword results
      *
      * @return mixed
