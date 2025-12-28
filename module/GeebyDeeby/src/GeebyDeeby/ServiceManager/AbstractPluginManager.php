a183c6baf71d033f3c0db661bb9cad46d060dc21
Demian Katz <demian.katz@villanova.edu>
2025-12-28 13:01:57 -0500

    Minor php-cs-fixer updates.

--- module/GeebyDeeby/src/GeebyDeeby/ServiceManager/AbstractPluginManager.php
+++ module/GeebyDeeby/src/GeebyDeeby/ServiceManager/AbstractPluginManager.php
@@ -30,8 +30,7 @@
 namespace GeebyDeeby\ServiceManager;
 
 use Laminas\ServiceManager\AbstractPluginManager as Base;
-use Laminas\ServiceManager\Exception\RuntimeException
-as ServiceManagerRuntimeException;
+use Laminas\ServiceManager\Exception\RuntimeException as ServiceManagerRuntimeException;
 
 /**
  * GeebyDeeby Plugin Manager
