5f3d866b32999bb6a3742e5f283b8843dcff29dd
Demian Katz <demian.katz@villanova.edu>
2025-12-29 18:40:22 -0500

    Add PHPUnit.

---
+++ module/GeebyDeeby/tests/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Bootstrap logic for PHPUnit
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Demian Katz 2025.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category VuFind
+ * @package  Tests
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/demiankatz/Geeby-Deeby Main Site
+ */
+
+define('APPLICATION_PATH', __DIR__ . '/../../..');
+chdir(APPLICATION_PATH);
+
+// Composer autoloading
+if (file_exists('vendor/autoload.php')) {
+    include 'vendor/autoload.php';
+}
