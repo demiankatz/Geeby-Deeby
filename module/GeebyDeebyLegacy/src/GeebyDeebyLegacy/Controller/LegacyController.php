59c2afabdd9c401ab0d15bfe2b0068d8f9650081
Demian Katz <demian.katz@villanova.edu>
2026-01-17 18:28:15 -0500

    Update comments.

--- module/GeebyDeebyLegacy/src/GeebyDeebyLegacy/Controller/LegacyController.php
+++ module/GeebyDeebyLegacy/src/GeebyDeebyLegacy/Controller/LegacyController.php
@@ -3,7 +3,7 @@
 /**
  * Legacy controller (for compatibility with old gamebooks.org filenames)
  *
- * PHP version 5
+ * PHP version 8
  *
  * Copyright (C) Demian Katz 2012.
  *
@@ -17,8 +17,8 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
  *
  * @category GeebyDeeby
  * @package  Controller
