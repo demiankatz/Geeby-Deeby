c398d0115fa8e2aa5cbf90257295d5f72ab4eeac
Demian Katz <demian.katz@villanova.edu>
2026-02-13 16:51:44 -0500

    Create Country entity.

---
+++ module/GeebyDeeby/src/GeebyDeeby/Db/Entity/Country.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Country entity model.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Demian Katz 2026.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+
+namespace GeebyDeeby\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Country entity model.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+#[ORM\Table(name: 'Countries')]
+#[ORM\Entity]
+class Country extends AbstractEntity implements CountryEntityInterface
+{
+    /**
+     * Unique ID.
+     *
+     * @var int
+     */
+    #[ORM\Column(name: 'Country_ID', type: 'integer', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    protected int $id;
+
+    /**
+     * Country name.
+     *
+     * @var string
+     */
+    #[ORM\Column(name: 'Country_Name', type: 'text', length: 255, nullable: false)]
+    protected string $countryName;
+
+    /**
+     * Get identifier (returns null for an uninitialized or non-persisted object).
+     *
+     * @return ?int
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the name of the country.
+     *
+     * @return string
+     */
+    public function getCountryName(): string
+    {
+        return $this->countryName;
+    }
+
+    /**
+     * Set the name of the country.
+     *
+     * @param string $name New name.
+     *
+     * @return static
+     */
+    public function setCountryName(string $name): static
+    {
+        $this->countryName = $name;
+        return $this;
+    }
+}
