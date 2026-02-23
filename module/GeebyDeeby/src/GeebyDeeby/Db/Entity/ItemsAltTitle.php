505fc3b10e93c0efba1b8864a222c6fef07713da
Demian Katz <demian.katz@villanova.edu>
2026-02-23 15:33:16 -0500

    Create alt title entities.

---
+++ module/GeebyDeeby/src/GeebyDeeby/Db/Entity/ItemsAltTitle.php
@@ -0,0 +1,188 @@
+<?php
+
+/**
+ * Item alternate title entity model.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Demian Katz 2026.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+
+namespace GeebyDeeby\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Item alternate title entity model.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+#[ORM\Table(name: 'Items_AltTitles')]
+#[ORM\Index(name: 'item_id', columns: ['Item_ID'])]
+#[ORM\Index(name: 'note_id', columns: ['Note_ID'])]
+#[ORM\Entity]
+#[ORM\EntityListeners([Feature\EntityManagerInjector::class])]
+class ItemsAltTitle extends AbstractEntity implements ItemsAltTitleEntityInterface
+{
+    use Feature\EntityManagerAwareTrait;
+
+    /**
+     * Unique ID.
+     *
+     * @var int
+     */
+    #[ORM\Column(name: 'Sequence_ID', type: 'bigint', nullable: false)]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'IDENTITY')]
+    protected int $id;
+
+    /**
+     * Item alternate name.
+     *
+     * @var string
+     */
+    #[ORM\Column(name: 'Item_AltName', type: 'text', length: 65535, nullable: false)]
+    protected string $altName;
+
+    /**
+     * Item.
+     *
+     * @var Item
+     */
+    #[ORM\JoinColumn(
+        name: 'Item_ID',
+        referencedColumnName: 'Item_ID',
+        nullable: false,
+        options: ['default' => 0]
+    )]
+    #[ORM\ManyToOne(targetEntity: Item::class)]
+    protected Item $item;
+
+    /**
+     * Note.
+     *
+     * @var Note
+     */
+    #[ORM\JoinColumn(
+        name: 'Note_ID',
+        referencedColumnName: 'Note_ID',
+        nullable: true,
+        options: ['default' => null]
+    )]
+    #[ORM\ManyToOne(targetEntity: Note::class)]
+    protected Note $note;
+
+    /**
+     * Get identifier (returns null for an uninitialized or non-persisted object).
+     *
+     * @return ?int
+     */
+    public function getId(): ?int
+    {
+        return $this->id ?? null;
+    }
+
+    /**
+     * Get the alternate name associated with the item.
+     *
+     * @return string
+     */
+    public function getAltName(): string
+    {
+        return $this->altName;
+    }
+
+    /**
+     * Set the alternate name associated with the item.
+     *
+     * @param string $name New name.
+     *
+     * @return static
+     */
+    public function setAltName(string $name): static
+    {
+        $this->altName = $name;
+        return $this;
+    }
+
+    /**
+     * Get associated item.
+     *
+     * @return ItemEntityInterface
+     */
+    public function getItem(): ItemEntityInterface
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set associated item.
+     *
+     * @param int|ItemEntityInterface $item Associated item entity or ID
+     *
+     * @return static
+     */
+    public function setItem(int|ItemEntityInterface $item): static
+    {
+        if ($item instanceof Item) {
+            $this->item = $item;
+            return $this;
+        } elseif ($item instanceof ItemEntityInterface) {
+            $item = $item->getId();
+        }
+        $this->item = $this->entityManager->getReference(Item::class, $item);
+        return $this;
+    }
+
+    /**
+     * Get associated note (if any).
+     *
+     * @return ?NoteEntityInterface
+     */
+    public function getNote(): ?NoteEntityInterface
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set associated note (if any).
+     *
+     * @param int|NoteEntityInterface|null $note Associated note entity or ID (null for none)
+     *
+     * @return static
+     */
+    public function setNote(int|NoteEntityInterface|null $note): static
+    {
+        if ($note instanceof Note || null === $note) {
+            $this->note = $note;
+            return $this;
+        } elseif ($note instanceof NoteEntityInterface) {
+            $note = $note->getId();
+        }
+        $this->note = $this->entityManager->getReference(Note::class, $note);
+        return $this;
+    }
+}
