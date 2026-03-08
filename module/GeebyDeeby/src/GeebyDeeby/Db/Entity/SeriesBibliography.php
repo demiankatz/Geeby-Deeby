19b174d46cfc42969dd2478c67b9958f7f9e98e7
Demian Katz <demian.katz@villanova.edu>
2026-03-08 15:26:38 -0400

    Add bibliography entities.

---
+++ module/GeebyDeeby/src/GeebyDeeby/Db/Entity/SeriesBibliography.php
@@ -0,0 +1,139 @@
+<?php
+
+/**
+ * Item relationship value entity model.
+ *
+ * PHP version 8
+ *
+ * Copyright (C) Demian Katz 2026.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see
+ * <https://www.gnu.org/licenses/>.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+
+namespace GeebyDeeby\Db\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Series relationship value entity model.
+ *
+ * @category GeebyDeeby
+ * @package  Database
+ * @author   Demian Katz <demian.katz@villanova.edu>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://vufind.org/wiki/development:plugins:database_gateways Wiki
+ */
+#[ORM\Table(name: 'Series_Bibliography')]
+#[ORM\Index(name: 'idx_56719ff37e63b755', columns: ['Series_ID'])]
+#[ORM\Index(name: 'item_id', columns: ['Item_ID'])]
+#[ORM\Entity]
+#[ORM\EntityListeners([Feature\EntityManagerInjector::class])]
+class SeriesBibliography extends AbstractEntity implements SeriesBibliographyEntityInterface
+{
+    use Feature\EntityManagerAwareTrait;
+
+    /**
+     * Source series.
+     *
+     * @var Series
+     */
+    #[ORM\JoinColumn(
+        name: 'Series_ID',
+        referencedColumnName: 'Series_ID',
+        nullable: false,
+        options: ['default' => 0]
+    )]
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Series::class)]
+    protected Series $series;
+
+    /**
+     * Bibliography item.
+     *
+     * @var Item
+     */
+    #[ORM\JoinColumn(
+        name: 'Item_ID',
+        referencedColumnName: 'Item_ID',
+        nullable: false,
+        options: ['default' => 0]
+    )]
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Item::class)]
+    protected Item $item;
+
+    /**
+     * Get the subject series.
+     *
+     * @return SeriesEntityInterface
+     */
+    public function getSeries(): SeriesEntityInterface
+    {
+        return $this->series;
+    }
+
+    /**
+     * Set the subject series.
+     *
+     * @param int|SeriesEntityInterface $series Associated series entity or ID
+     *
+     * @return static
+     */
+    public function setSeries(int|SeriesEntityInterface $series): static
+    {
+        if ($series instanceof Series) {
+            $this->series = $series;
+            return $this;
+        } elseif ($series instanceof SeriesEntityInterface) {
+            $series = $series->getId();
+        }
+        $this->series = $this->entityManager->getReference(Series::class, $series);
+        return $this;
+    }
+
+    /**
+     * Get item about the subject series.
+     *
+     * @return ItemEntityInterface
+     */
+    public function getItem(): ItemEntityInterface
+    {
+        return $this->item;
+    }
+
+    /**
+     * Set item about the subject series.
+     *
+     * @param int|ItemEntityInterface $item Associated item entity or ID
+     *
+     * @return static
+     */
+    public function setItem(int|ItemEntityInterface $item): static
+    {
+        if ($item instanceof Item) {
+            $this->item = $item;
+            return $this;
+        } elseif ($item instanceof ItemEntityInterface) {
+            $item = $item->getId();
+        }
+        $this->item = $this->entityManager->getReference(Item::class, $item);
+        return $this;
+    }
+}
